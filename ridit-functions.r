ced7e0e2b7c688d3106b7640b2164ad9607dfd08
Eric Bohlman <ebohlman@omsdev.com>
2018-03-06 02:20:01 -0600

    Initial commit

---
+++ ridit-functions.r
@@ -0,0 +1,33 @@
+# Convert vector of counts to ridits
+
+to.ridit <- function(v) {
+  (cumsum(v) - .5 * v) / sum(v)
+}
+
+# Calculate mean ridit for vector of counts relative to reference group
+
+mean.ridit <- function(v, ref) {
+  sum(to.ridit(ref) * v ) / sum(v)
+}
+
+# Calculate mean ridits for several groups
+# x is matrix of counts
+# margin is 1 for groups in rows, 2 for groups in columns
+# If ref is ommitted, totals across groups are used as reference group
+# If ref is a vector of counts, it's used as reference group
+# Otherwise, ref is the number of the group to use as reference
+
+ridits <- function(x, margin, ref=NULL) {
+  if (length(ref) > 1) {
+    refgroup <- ref
+  } else if (length(ref) == 1) {
+    if (margin==1) {
+      refgroup <- x[ref,]
+    } else {
+      refgroup <- x[, ref]
+    }
+  } else {
+    refgroup <- apply(x, 3-margin, sum)
+  }
+  apply(x, margin, mean.ridit, refgroup)
+}
