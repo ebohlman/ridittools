56b0662d82fbaa3707793bd1ef241411ff933fec
Eric Bohlman <ebohlman@omsdev.com>
2018-03-06 07:02:50 -0600

    Added functions to compute standard errors

--- ridit-functions.r
+++ ridit-functions.r
@@ -10,13 +10,24 @@ mean.ridit <- function(v, ref) {
   sum(to.ridit(ref) * v ) / sum(v)
 }
 
+# Calculate standard error of mean ridit for vector of counts relative to reference group
+
+se.ridit <- function(v, ref) {
+  N <- sum(ref)
+  n <- sum(v)
+  term1 <- (n + 1) / N
+  term2 <- 1 / (N * (N + n - 1))
+  term3 <- sum((ref + v) ^ 3) / (N * (N + n) * (N + n - 1))
+  (1 / (2 * sqrt(3 * n))) * sqrt(1 + term1 + term2 - term3)
+}
+
 # Utility to determine reference group
 
-ridits.refgroup <- function(x, margin, ref=NULL {
+ridits.refgroup <- function(x, margin, ref=NULL) {
   if (length(ref) > 1) {
     refgroup <- ref
   } else if (length(ref) == 1) {
-    if (margin==1) {
+    if (margin==1) {system.time(
       refgroup <- x[ref,]
     } else {
       refgroup <- x[, ref]
@@ -35,4 +46,11 @@ ridits.refgroup <- function(x, margin, ref=NULL {
 
 ridits <- function(x, margin, ref=NULL) {
   apply(x, margin, mean.ridit, ridits.refgroup(x,margin,ref))
+}
+
+# Calculate standard errors of mean ridits for several groups
+# Arguments are same as for ridits()
+
+ridits.se <- function(x, margin, ref=NULL) {
+  apply(x, margin, se.ridit, ridits.refgroup(x,margin,ref))
 }
