9133a6381c21ba5030eb0b66188977b0d7768411
Eric Bohlman <ebohlman@omsdev.com>
2018-03-07 07:59:43 -0600

    Fix typo in last commit

--- ridit-functions.r
+++ ridit-functions.r
@@ -27,7 +27,7 @@ ridits.refgroup <- function(x, margin, ref=NULL) {
   if (length(ref) > 1) {
     refgroup <- ref
   } else if (length(ref) == 1) {
-    if (margin==1) (
+    if (margin==1) {
       refgroup <- x[ref,]
     } else {
       refgroup <- x[, ref]
